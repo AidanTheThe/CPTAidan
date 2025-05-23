894716052376b3a0cece6782088b5d6537ef1989
AidanTheThe <aidan.brown26@ycdsbk12.ca>
2025-05-23 09:38:16 -0400

    Prompts user to enter word then prints first letter

--- CPTAidan.java
+++ CPTAidan.java
@@ -3,6 +3,9 @@ import arc.*;
 public class CPTAidan{
 	public static void main(String[] args){
 		Console con = new Console();
-		con.println("Hello World");
+		con.println("Enter a word");
+		String strWord = con.readLine();
+		String strLetter = strWord.substring(0,1);
+		con.println("The first letter is");
 	}
 }

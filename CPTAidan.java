120b392004455a442673da2ca8362395ba9565bf
AidanTheThe <aidan.brown26@ycdsbk12.ca>
2025-05-27 09:59:01 -0400

    Update CPTAidan.java
    Main menu method complete

--- CPTAidan.java
+++ CPTAidan.java
@@ -3,13 +3,13 @@ import arc.*;
 public class CPTAidan{
 	public static void main(String[] args){
 		Console con = new Console();
-		startup(con);
+		menu(con);
 		String strModeInput;
 		strModeInput = con.readLine();
 	}
 	
-	// Startup Screen
-	public static void startup(Console con){
+	// Main Menu
+	public static void menu(Console con){
 		con.println("Type 1 to Play Game");
 		con.println("Type 2 to View Leaderboard");
 		con.println("Type 3 to Add Theme");

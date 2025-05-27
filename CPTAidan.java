aa6d8e89b132cb7d7d0510b4a66adab8ca6d7581
AidanTheThe <aidan.brown26@ycdsbk12.ca>
2025-05-27 09:58:10 -0400

    Update CPTAidan.java
    Main menu screen

--- CPTAidan.java
+++ CPTAidan.java
@@ -3,13 +3,15 @@ import arc.*;
 public class CPTAidan{
 	public static void main(String[] args){
 		Console con = new Console();
-		
-		
+		startup(con);
+		String strModeInput;
+		strModeInput = con.readLine();
 	}
 	
+	// Startup Screen
 	public static void startup(Console con){
 		con.println("Type 1 to Play Game");
-		con.println("Type 2 to Vjew Leaderboard");
+		con.println("Type 2 to View Leaderboard");
 		con.println("Type 3 to Add Theme");
 		con.println("Type 4 to Quit");
 	}

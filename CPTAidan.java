57b9c50b2421e0954a2e571b6a1cf65f0389c842
AidanTheThe <aidan.brown26@ycdsbk12.ca>
2025-05-29 09:11:09 -0400

    Changed screen resolution, added title, created help and secret menu

--- CPTAidan.java
+++ CPTAidan.java
@@ -2,8 +2,9 @@ import arc.*;
 
 public class CPTAidan{
 	public static void main(String[] args){
-		Console con = new Console();
-		CPTAidanTools.menu(con);
+		Console con = new Console("Hangman", 1280, 720);
+		CPTAidanTools.mainMenu(con);
+		System.out.println("Menu Loaded");
 		String strModeInput;
 		strModeInput = con.readLine();
 	}
--- CPTAidanTools.java
+++ CPTAidanTools.java
@@ -2,18 +2,38 @@ import arc.*;
 
 public class CPTAidanTools{
 	
+	// Assistance Screens/Menus
 	// Main Menu
-	public static void menu(Console con){
+	public static void mainMenu(Console con){
 		con.println("Type P to Play Game");
 		con.println("Type V to View Leaderboard");
 		con.println("Type A to Add Theme");
 		con.println("Type Q to Quit");
 		con.println("Type H for Help");
 	}
+	// Help Screen
+	public static void helpScreen(Console con){
+		con.println("Help");
+		con.println("\nHow To Play");
+		con.println("Each line represents a missing letter");
+		con.println("Guess the word by typing it out and pressing Enter");
+		con.println("For every wrong guess, a new random letter is revealed and a body part is added to the stickman");
+		con.println("The goal is to guess the word before the stickman is fully drawn");
+		con.println("\nType R to Return to the Main Menu");
+	}
+	// Secret Menu
+	public static void secretMenu(Console con){
+		con.println("I asked the IT guy, 'how do you make a motherboard?'");
+		con.println("He said, 'I tell her about my job.'");
+		con.println("\nI started a band called 999 Megabytes");
+		con.println("We haven't gotten a gig yet");
+		con.println("\nWhy do programmers prefer dark mode?");
+		con.println("Because light attracts bugs!");
+	}
 	
 	// Hangman Drawings 1-7
 	// Drawing 1: No body
-	public static void Drawing1(Console con){
+	public static void drawing1(Console con){
 		con.println("    /----------|");
 		con.println("    |          |");
 		con.println("    |");
@@ -23,7 +43,7 @@ public class CPTAidanTools{
 		con.println("- - - - -");
 	}
 	// Drawing 2: Head
-	public static void Drawing2(Console con){
+	public static void drawing2(Console con){
 		con.println("    /----------|");
 		con.println("    |          |");
 		con.println("    |          O");
@@ -33,7 +53,7 @@ public class CPTAidanTools{
 		con.println("- - - - -");
 	}
 	// Drawing 3: Head + Body
-	public static void Drawing3(Console con){
+	public static void drawing3(Console con){
 		con.println("    /----------|");
 		con.println("    |          |");
 		con.println("    |          O");
@@ -43,7 +63,7 @@ public class CPTAidanTools{
 		con.println("- - - - -");
 	}
 	// Drawing 4: Head + Body + Left Arm
-	public static void Drawing4(Console con){
+	public static void drawing4(Console con){
 		con.println("    /----------|");
 		con.println("    |          |");
 		con.println("    |          O");
@@ -53,7 +73,7 @@ public class CPTAidanTools{
 		con.println("- - - - -");
 	}
 	// Drawing 5: Head + Body + Arms
-	public static void Drawing5(Console con){
+	public static void drawing5(Console con){
 		con.println("    /----------|");
 		con.println("    |          |");
 		con.println("    |          O");
@@ -63,7 +83,7 @@ public class CPTAidanTools{
 		con.println("- - - - -");
 	}
 	// Drawing 6: Head + Body + Arms + Left Leg
-	public static void Drawing6(Console con){
+	public static void drawing6(Console con){
 		con.println("    /----------|");
 		con.println("    |          |");
 		con.println("    |          O");
@@ -73,7 +93,7 @@ public class CPTAidanTools{
 		con.println("- - - - -");
 	}
 	// Drawing 7: Head + Body + Arms + Legs
-	public static void Drawing7(Console con){
+	public static void drawing7(Console con){
 		con.println("    /----------|");
 		con.println("    |          |");
 		con.println("    |          O");
